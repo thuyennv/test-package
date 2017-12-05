9b91642bc8f21dca584ea4869bd80e17c82593d7
thuyennv <thuyennv.hust@gmail.com>
2017-12-05 08:46:45 +0700

    first commit

---
+++ src/Controllers/PackageController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Test\Package\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PackageController extends Controller
+{
+    //
+}
---
+++ src/Routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+Route::namespace('Test\Package')->as('package::')->middleware('web')->group(function () {
+    // Routes defined here have the web middleware applied
+    // like the web.php file in a laravel project
+    // They also have an applied controller namespace and a route names.
+
+    Route::middleware('package')->group(function () {
+        // Routes defined here have the self-assigned middleware applied.
+        // By default this middleware is empty.
+    });
+});
